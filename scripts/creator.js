fdda45de124e220936745747ffbf87573ec0ed44
Joseph Kessler <40746620+jxkessler@users.noreply.github.com>
2019-05-20 15:18:07 -0400

    Expense functionality
    Made add button work
    Made delete button work

--- scripts/creator.js
+++ scripts/creator.js
@@ -3,6 +3,12 @@ var incomeSourceField, incomeAmountField, incomeSource, incomeAmount, incomeTota
 var incomeList, incomeListTemplate;
 var incomeArray;
 
+var expenseSourceField, expenseAmountField, expenseCategoryField, expenseTotalField, expenseSource, expenseAmount, expenseCategory, expenseTotal;
+var expenseList, expenseListTempalte;
+var expenseArray;
+
+var housingArray, utilitiesArray, transportationArray, foodArray, personalArray, debtArray, otherArray;
+
 // INITIALIZE //
 init();
 
@@ -18,6 +24,26 @@ function init() {
     incomeTotal = 0;
 
     incomeArray = [];
+
+    expenseSourceField = document.getElementById("expense-source");
+    expenseAmountField = document.getElementById("expense-amount");
+    expenseCategoryField = document.getElementById("expense-categories");
+
+    expenseList = document.getElementById("expense-list");
+    expenseListTempalte = document.getElementById("expense-list-item-template");
+
+    expenseTotalField = document.getElementById("expense-total");
+    expenseTotal = 0;
+
+    expenseArray = [];
+
+    housingArray = [];
+    utilitiesArray = [];
+    transportationArray = [];
+    foodArray = [];
+    personalArray = [];
+    debtArray = [];
+    otherArray = [];
 }
 
 function addIncome() {
@@ -28,6 +54,12 @@ function addIncome() {
 
 }
 
+function addExpense() {
+    if(checkExpenseFields()){
+        buildExpenseTemplate();
+    }
+}
+
 function deleteIncome(button) {
     //Get li ID
     var liID = button.parentNode.id;
@@ -36,8 +68,36 @@ function deleteIncome(button) {
     var listItem = document.getElementById(liID);
     incomeList.removeChild(listItem);
 
+    //Update income total
+    var income = incomeArray.filter( function(obj) {
+        return obj.id == liID;
+    });
+    incomeTotal = incomeTotal - income[0].amount;
+    incomeTotalField.innerHTML = "Income Total: $" + incomeTotal;
+
     //Remove item from array. Uses a filter to update the array with anything that doesn't have the same ID
     incomeArray = incomeArray.filter( function(obj) {
+        return obj.id != liID;
+    });
+}
+
+function deleteExpense(button) {
+    //Get li ID
+    var liID = button.parentNode.id;
+
+    //Remove li from ul
+    var listItem = document.getElementById(liID);
+    expenseList.removeChild(listItem);
+
+    //Update income total
+    var expense = expenseArray.filter( function(obj) {
+        return obj.id == liID;
+    });
+    expenseTotal = expenseTotal - expense[0].amount;
+    expenseTotalField.innerHTML = "Expense Total: $" + expenseTotal;
+
+    //Remove item from array. Uses a filter to update the array with anything that doesn't have the same ID
+    expenseArray = expenseArray.filter( function(obj) {
         return obj.id != liID;
     });
 }
@@ -61,6 +121,27 @@ function checkIncomeFields() {
     }else{
         return true;
     }
+}
+
+function checkExpenseFields(){
+    expenseSource = expenseSourceField.value;
+    expenseAmount = expenseAmountField.value;
+
+    if(expenseSource == ""){
+        alert("You need to enter an expense source. ");
+        return false;
+    }else
+    if(expenseAmount == "") {
+        alert("You need to enter an expense amount. ");
+        return false;
+    }else
+    if(!expenseAmount.match(/^[0-9]\d*(\.\d+)?$/)){
+        alert("The expense amount needs to be a number that's greater than 0. ");
+        return false;
+    }else{
+        return true;
+    }
+
 }
 
 //Sets up the list item based on the income list item template
@@ -91,10 +172,46 @@ function buildIncomeTemplate() {
 
     //Update total
     incomeTotal = incomeTotal + parseFloat(incomeAmount);
-    incomeTotalField.innerHTML = "Income Total: $" + incomeAmount;
+    incomeTotalField.innerHTML = "Income Total: $" + incomeTotal;
 
     //Clear fields
     incomeAmountField.value = "";
     incomeSourceField.value = "";
+
+}
+
+function buildExpenseTemplate() {
+    //Clone template
+    var newTemplate = expenseListTempalte.content.cloneNode(true);
+    var paragraph = newTemplate.querySelector("p")
+    var listItem  = newTemplate.querySelector("li");
+
+    //Format income
+    expenseAmount = parseFloat(expenseAmount).toFixed(2);
+
+    //Create income object and add to array
+    var expense = {
+        source: expenseSource,
+        amount: expenseAmount,
+        optgroup: expenseCategoryField.options[expenseCategoryField.selectedIndex].parentNode.label, //Gets the optgroup label
+        option: expenseCategoryField.value,
+        id: "expense_" + expenseArray.length
+    }
+    expenseArray.push(expense);
+
+    //Set the ID
+    listItem.setAttribute("id", expense.id);
+
+    //Update the nodes in the template
+    paragraph.innerHTML = "$" + expenseAmount + " from " + expenseSource;
+    expenseList.append(newTemplate);
+
+    //Update total
+    expenseTotal = expenseTotal + parseFloat(expenseAmount);
+    expenseTotalField.innerHTML = "Expense Total: $" + expenseTotal;
+
+    //Clear fields
+    expenseAmountField.value = "";
+    expenseSourceField.value = "";
 
 }
